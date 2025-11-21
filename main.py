2a7d447163f1a9f523a35ad66dcf7247d06fbb45
WarlordDK <krsdiman@gmail.com>
2025-11-21 18:28:54 +0500

    cors middleware

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.router import router as router_analyze
 import uvicorn
 
 app = FastAPI()
 app.include_router(router_analyze)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def home_page():
